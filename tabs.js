29e856f97653cd92c0544ffb051fd94c9bf7f625
Inna-B10 <148863587+Inna-B10@users.noreply.github.com>
2023-12-17 05:15:12 +0100

    Add files via upload
    updated files tips.html, tips.css, tabs.js

--- tabs.js
+++ tabs.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function () {
-  jQuery(".tabs .tab-links a").on("click", function (e) {
+  jQuery(".tabs-section .tab-links a").on("click", function (e) {
     var currentAttrValue = jQuery(this).attr("href");
 
     // Show/Hide Tabs
-    jQuery(".tabs " + currentAttrValue)
+    jQuery(".tabs-section " + currentAttrValue)
       .show()
       .siblings()
       .hide();
@@ -18,23 +18,3 @@ jQuery(document).ready(function () {
     e.preventDefault();
   });
 });
-
-// When the user scrolls the page, execute myFunction
-// window.onscroll = function () {
-//   myFunction();
-// };
-
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.scrollY >= sticky) {
-//     navbar.classList.add("sticky");
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
